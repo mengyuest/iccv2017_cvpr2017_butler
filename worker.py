ca18325fa29634c1bbd77f33cac0201d718888a0
Meng <sc071139@126.com>
2017-08-25 09:53:53 +0800

    download csv stat file from web html, not local anymore

--- worker.py
+++ worker.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python  
+# -*- coding: utf-8 -*-  
+
 import csv
 import os
 import shutil
 import errno
 from multiprocessing import Process, Pool, Value, Manager
 from datetime import datetime
-
+import urllib2
 
 def download(todoitem):
 	paper_dir = todoitem[0]
@@ -12,9 +15,11 @@ def download(todoitem):
 	paper_path = todoitem[2]
 	dtype = todoitem[3]
 	if dtype == "paper":
+
 		os.system("mwget -d \""+paper_dir+"\" -n " + str(connect_num.value) + \
 			" -t 90 http://openaccess.thecvf.com/content_cvpr_2017/papers/"+paper_path)
 	if dtype == "appendix":
+
 		os.system("mwget -d \""+paper_dir+"\" -n " + str(connect_num.value) + \
 			" -t 90 http://openaccess.thecvf.com/content_cvpr_2017/supplemental/"+paper_path)
 
@@ -28,9 +33,29 @@ def download(todoitem):
 		"\\033[0m \"")
 
 
+def standarize_html(doc):
+	doc = doc.replace("“", "\"")
+	doc = doc.replace("”", "\"")
+	doc = doc.replace("—", "-")
+	doc = doc.replace("°", "deg")
+	doc = doc.replace("é", "e")
+	doc = doc.replace("&", "and")
+	return doc
+
+def standarize_csv(doc):
+
+	doc = doc.replace("\,", "$")
+	doc = doc.replace("," , "\t")
+	doc = doc.replace("$" , ",")
+	doc = doc.replace("\\\"", "\"")
+	doc = doc.replace("--", "-")
+	doc = doc.replace("&", "and")
+	return doc
+
 if __name__ == "__main__":
 
 	dt_start = datetime.now()
+
 	server = Manager()
 	process_num = 50
 	connect_num = server.Value('i', 4)
@@ -46,17 +71,78 @@ if __name__ == "__main__":
 	src_path = root_path+"Zi Other Files/" #server.Value(c_char_p, root_path+"Zi Other Files/")
 	paper_dir = root_path+"papers/" #server.Value(c_char_p, root_path+"papers/")
 
+	if not os.path.exists(root_path):
+		try:
+			os.makedirs(root_path)
+		except OSError as exception:
+			if exception.errno != errno.EEXIST:
+				raise
+
+	if not os.path.exists(src_path):
+		try:
+			os.makedirs(src_path)
+		except OSError as exception:
+			if exception.errno != errno.EEXIST:
+				raise
+
+	os.system("mwget -d \""+src_path+"\" -n 4 "  + \
+			"-t 90 http://openaccess.thecvf.com/content_cvpr_2017/3-cvpr2017-papers.csv")
+
+	print("Downloading schedule csv files ...")
+	stat_csv = urllib2.urlopen('http://cvpr2017.thecvf.com/program/main_conference')
+	w1 = open(src_path + "origin_stat.html", 'wb') 
+	w1.write(stat_csv.read())
+	w1.close()
+
+	w1 = open(src_path + "origin_stat.html", 'r') 
+	w2 = open(src_path + "stat.csv", 'wb')
+
+	pencil = csv.writer(w2, delimiter = '\t', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
+	pencil.writerow(['TOPIC',	'SESSION',	'ID',	'PAPER',	'AUTHOR'])
+
+	csv_content =   ['NULL','NULL','NULL','NULL','NULL','NULL','NULL','NULL','NULL']
+
+	lines = w1.readlines()
+
+	col_id=0
+	start_trim = False
+
+	for line in lines:
+		if start_trim == True:
+			if  "</table>" in line:
+				break
+			else:	
+				if "</tr" in line and csv_content[5] != "NULL":
+					col_id = 0
+					pencil.writerow([csv_content[5],csv_content[4],csv_content[6],csv_content[7],csv_content[8]])
+
+				elif "<td " in line:
+					#print("@@  "+line)
+					strs = line.strip().split('>')
+					for item in strs:
+						#print("\""+item+"\"")
+						item = item.strip()
+						if item != "" and item[0] != '<':
+							csv_content[col_id] = standarize_html(item.strip().split('<')[0].strip())
+							break
+					#print("id="+str(col_id))
+					col_id = col_id + 1
+
+		elif "\"program_schedule\"" in line:
+			start_trim = True
+
+		else:
+			continue
+
+	w2.close()
+	w1.close()
+
 	f1 = open(src_path + "stat.csv",'r')
 	f2 = open(src_path + "3-cvpr2017-papers.csv",'r')
 	f3 = open(src_path + "cvpr2017.csv",'wb')
 
 	doc = f2.read()
-	doc = doc.replace("\,", "$")
-	doc = doc.replace("," , "\t")
-	doc = doc.replace("$" , ",")
-	doc = doc.replace("\\\"", "\"")
-	doc = doc.replace("--", "-")
-	doc = doc.replace("&", "and")
+	doc = standarize_csv(doc)
 
 	f3.write(doc)
 
@@ -141,10 +227,6 @@ if __name__ == "__main__":
 					
 					break
 
-					
-
-
-
 	f1.close()
 	f3.close()
 
@@ -164,8 +246,4 @@ if __name__ == "__main__":
 	download_speed = file_size.value/1024/time_interval
 	os.system("echo \"\\033[1;32mElapse time: "+(str)(time_interval)+"s"+ "\\033[0m \"")
 	os.system("echo \"\\033[1;32mDownload speeds: "+ (str)(download_speed) +"KB/s"+ "\\033[0m \"")
-
-
-
-
-
+	
